9340ab134561b7b335f9454e713a49154f0f3fa6
Simran2004-rana <pinkurana202@gmail.com>
2025-08-02 13:48:23 +0530

    Coded for index

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import issueRoutes from './routes/issueRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch(err => console.log('Mongo Error:', err));
+
+app.use('/api/issues', issueRoutes);
+app.use('/api/admin', adminRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
